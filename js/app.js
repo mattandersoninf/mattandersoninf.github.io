86f736a104a39b7e9e07b84b461fb5254bdca0c4
Matt Anderson <matt.anderson.inf@gmail.com>
2020-01-05 14:05:32 -0500

    The table animation works for individual cells
    The table animation works for individual cells, expanding the pulse function

--- js/app.js
+++ js/app.js
@@ -3,22 +3,32 @@
 // table container
 const animatedTable = document.querySelector("table.board");
 
-// animated table event listener
+/*************************************************************
+ *  ANIMATED TABLE EVENT LISTENER
+ *  If the element pressed is a table cell and that table cell
+ *  isn't already animated, then call the pulsePeach function.
+ * ***********************************************************/
 animatedTable.addEventListener("click", function(){
     
     if (event.target.tagName == "TD" && !(event.target.classList.contains("animate-table-cell"))){
         
-        pulsePeachColor(event.target);
+        pulsePeachColor(event.target, 4);
     
     }
 
 });
 
-
-function pulsePeachColor(tableCell){
-    event.target.classList.add("animate-table-cell");
-    console.log(tableCell.classList);
-    // setTimeout(tableCell.classList.remove("animate-table-cell"),2000);
-    console.log(tableCell.classList);
+/*************************************************************
+ *  PULSE PEACH FUNCTION
+ *  
+ * ***********************************************************/
+function pulsePeachColor(tableCell, pulseDepth){
+    let tableCellCoordinates = [tableCell.id.substring(0,tableCell.id.indexOf("-")), tableCell.id.substring(tableCell.id.indexOf("-")+1, tableCell.id.length)];
+    if(pulseDepth > -1){
+        tableCell.classList.add("animate-table-cell");
+        window.setTimeout(function() {
+            tableCell.classList.remove('animate-table-cell');
+        },1500);
+    }
 
 }
