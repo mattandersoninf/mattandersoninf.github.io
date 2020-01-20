529cb9765997228851a47d02e3b52590e9ea5b1f
Matt Anderson <matt.anderson.inf@gmail.com>
2020-01-19 23:20:14 -0500

    pulse function does not repeat towards pressed cell
    pulse function does not repeat towards pressed cell

--- js/app.js
+++ js/app.js
@@ -44,23 +44,28 @@ animatedTable.addEventListener("click", function(){
  * ***********************************************************/
 function pulsePeachColor(rowNum, colNum, pulseDepth){
     
-    if(pulseDepth > -1 && verifyCoordinates(rowNum, colNum)){
+  if(pulseDepth > -1 && verifyCoordinates(rowNum, colNum)){
 
-        document.getElementById(rowNum+"-"+colNum).classList.add("animate-table-cell");
-        
-        window.setTimeout(function() {
-            document.getElementById(rowNum+"-"+colNum).classList.remove('animate-table-cell');
+      if(!(document.getElementById(rowNum+"-"+colNum).classList.contains("animate-table-cell"))){
+          
+          document.getElementById(rowNum+"-"+colNum).classList.add("animate-table-cell");
 
-        },500);
+          
+          window.setTimeout(function() {
+              document.getElementById(rowNum+"-"+colNum).classList.remove('animate-table-cell');
 
-        window.setTimeout(function() {
-            pulsePeachColor(parseInt(rowNum,10)+1,parseInt(colNum,10),pulseDepth-1);
-            pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)+1,pulseDepth-1);
-            pulsePeachColor(parseInt(rowNum,10)-1,parseInt(colNum,10),pulseDepth-1);
-            pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)-1,pulseDepth-1); 
-        },200); 
-        
-    }
+          },500);
+
+          window.setTimeout(function() {
+              pulsePeachColor(parseInt(rowNum,10)+1,parseInt(colNum,10),pulseDepth-1);
+              pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)+1,pulseDepth-1);
+              pulsePeachColor(parseInt(rowNum,10)-1,parseInt(colNum,10),pulseDepth-1);
+              pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)-1,pulseDepth-1); 
+          },200); 
+
+      }
+      
+  }
 
 }
 
